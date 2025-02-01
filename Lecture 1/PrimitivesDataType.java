cea355978561a796aa925188b4070007fc32b0b8
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-02-01 20:24:29 +0530

    first commit

---
+++ Lecture 1/PrimitivesDataType.java
@@ -0,0 +1,10 @@
+public class PrimitivesDataType {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'a';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 12345.6789;
+        long longestInteger = 1234566789L;
+        boolean isStudent = true;
+    }
+}
---
+++ Lecture 1/TypeCasting.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);    
+        // float num = input.nextFloat();
+        // System.out.println(num);
+
+
+        //Type Castying Method
+        int num = (int) (67.56f);
+        System.out.println(num);
+
+        //Automatic type promotion in expression
+        // int a = 257;
+        // byte b = (byte)(a); // 257 % 256 = 1
+
+        // byte a = 40;
+        // byte b = 50;
+        // byte c = 100;
+        // int d = a * b / c;
+
+        // System.out.println(d);
+
+    //     byte b =  50;
+    //     b = b * 2;
+    // 
+      // int number  = 'A';
+      System.out.println();
+    }
+}
